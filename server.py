5678ac37d6dfe01a2100511049c29b3dfa970d19
sarguna19 <31096475+sarguna19@users.noreply.github.com>
2017-08-28 11:30:12 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import web
+import xml.etree.ElementTree as ET
+import json
+from subprocess import call
+
+
+
+tree = ET.parse('user_data.xml')
+root = tree.getroot()
+
+urls = (
+    '/users', 'list_users',
+    '/users/(.*)', 'get_user',
+    '/playbook/run', 'run_playbook'
+)
+
+app = web.application(urls, globals())
+
+class run_playbook:
+    def POST(self):
+        data = web.data()
+        data = json.loads(data)
+        fname=data["file"]
+        call(["ansible-playbook", fname])
+        
+    def GET(self):
+        print("GET")
+
+
+class list_users:        
+    def GET(self):
+	output = 'users:[';
+	for child in root:
+                print 'child', child.tag, child.attrib
+                output += str(child.attrib) + ','
+	output += ']';
+        return output
+
+class get_user:
+    def GET(self, user):
+	for child in root:
+		if child.attrib['id'] == user:
+		    return str(child.attrib)
+
+if __name__ == "__main__":
+    app.run()
